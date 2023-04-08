d800b431ef519dd1ac3548ce446127822d2ff069
Padillanur Fatmah Azzahra <padillanurfatmahazzahra@gmail>
2023-04-08 09:56:29 +0700

    First Commit

--- app.py
+++ app.py
@@ -1,20 +1,22 @@
-import os 
-from os.path import join, dirname
-from dotenv import load_dotenv
-
 from flask import Flask, render_template, request, jsonify
 from pymongo import MongoClient
 
-dotenv_path = join(dirname(__file__), '.env')
+import os
+from os.path import join,dirname
+from dotenv import load_dotenv
+
+
+
+dotenv_path =join(dirname(_file_),'.env')
 load_dotenv(dotenv_path)
 
-MONGODB_URI = os.environ.get("MONGODB_URI")
-DB_NAME = os.environ.get("DB_NAME")
+MONGODB_URI=os.environ.get("MONGODB_URI")
+DB_NAME =os.environ.get("DB_NAME")
 
 client = MongoClient(MONGODB_URI)
 db = client[DB_NAME]
+app = Flask(_name_)
 
-app = Flask(__name__)
 
 @app.route('/')
 def home():
@@ -23,7 +25,7 @@ def home():
 @app.route("/bucket", methods=["POST"])
 def bucket_post():
     bucket_receive = request.form['bucket_give']
-    count = db.bucket.count_documents({})
+    count =db.bucket.count_documents({})
     num = count + 1
     doc = {
         'num':num,
@@ -40,19 +42,21 @@ def bucket_done():
         {'num': int(num_receive)},
         {'$set': {'done': 1}}
     )
-    return jsonify({'msg': 'update done!'})    
+    return jsonify({'msg': 'Update done!'})
+
+
 
 @app.route("/bucket", methods=["GET"])
 def bucket_get():
     buckets_list = list(db.bucket.find({},{'_id':False}))
-    return jsonify({'buckets': buckets_list})
+    return jsonify({'buckets':buckets_list})
 
 @app.route("/bucket/delete", methods=["POST"])
 def bucket_delete():
     num_receive = request.form["num_give"]
-    db.bucket.delete_one({'num':int(num_receive)})
-    bucket_list = list(db.bucket.find({},{'_id':False}))
-    return jsonify({'msg':'Delete done!','bucket':bucket_list})
-
-if __name__ == '__main__':
+    db.bucket.delete_one({'num': int (num_receive)})
+    bucket_list= list(db.bucket.find({},{'_id':False}))
+    return jsonify({'msq':'Delete done!','buckets':'bucket_list'})
+    
+if _name_ == '_main_':
    app.run('0.0.0.0', port=5000, debug=True)
